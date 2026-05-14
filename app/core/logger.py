ae2676aacb28bca8b15bd5947e8dab52d0882be8
Vynzaro <eskardev@gmail.com>
2026-05-14 08:46:31 -0400

    Create logger.py

---
+++ app/core/logger.py
@@ -0,0 +1,32 @@
+import logging
+
+from app.core.paths import LOG_FILE, ensure_app_dirs
+from app.core.version import APP_NAME, VERSION
+
+
+def setup_logger() -> logging.Logger:
+    ensure_app_dirs()
+
+    logger = logging.getLogger(APP_NAME)
+    logger.setLevel(logging.INFO)
+
+    if logger.handlers:
+        return logger
+
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8")
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    logger.info("%s %s started", APP_NAME, VERSION)
+
+    return logger
