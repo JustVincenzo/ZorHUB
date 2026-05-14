da0b6cd6c3621eb70712d925a972b0e12f8e3495
Vynzaro <eskardev@gmail.com>
2026-05-14 08:42:39 -0400

    Create app/core/version.py

---
+++ app/core/version.py
@@ -0,0 +1,10 @@
+APP_NAME = "ZorHUB"
+VERSION = "pα0.1"
+CODENAME = "Foundation"
+CHANNEL = "private-pre-alpha"
+PUBLIC_RELEASE = False
+STABILITY = "experimental"
+
+MAINTAINER = "Vynzaro / JustVincenzo"
+REPOSITORY_URL = "https://github.com/Vynzaro/ZorHUB"
+LICENSE_NAME = "GNU General Public License v3.0"
