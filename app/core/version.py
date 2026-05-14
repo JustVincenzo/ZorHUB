8c61616ffc86e0392f000c84160cfe328fe9ac6e
Vynzaro <eskardev@gmail.com>
2026-05-14 15:57:27 -0400

    Update version and codename in version.py

--- app/core/version.py
+++ app/core/version.py
@@ -1,10 +1,11 @@
 APP_NAME = "ZorHUB"
-VERSION = "pα0.1"
-CODENAME = "Foundation"
+VERSION = "pα0.2"
+CODENAME = "Consent & Components"
 CHANNEL = "private-pre-alpha"
 PUBLIC_RELEASE = False
 STABILITY = "experimental"
 
-MAINTAINER = "Vynzaro / JustVincenzo"
+MAINTAINER = "Vynzaro"
 REPOSITORY_URL = "https://github.com/Vynzaro/ZorHUB"
 LICENSE_NAME = "GNU General Public License v3.0"
+APPLICATION_ID = "io.github.vynzaro.ZorHUB"
